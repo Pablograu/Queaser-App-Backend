19936b603650137becac8fad268f260d4a7ff855
Pablograu <paugf89@gmail.com>
2019-08-13 08:55:17 +0200

    added all routes

--- server.js
+++ server.js
@@ -2,11 +2,76 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require ('mongoose')
+const todoRoutes = express.Router()
 const PORT = 4000
 
+let Todo = require('./todo.model')
 
 app.use(cors())
 app.use(bodyParser.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true})
+const connection = mongoose.connection
+
+connection.once('open', function(){
+  console.log('mongodb connection established succesfuly, a tope!');
+})
+
+//INDEX ROUTE
+todoRoutes.route('/').get(function(req,res){
+  Todo.find(function(err, todos){
+    if(err){
+      console.log(err);
+    } else {
+      res.json(todos)
+    }
+  })
+})
+
+//QUEASER ROUTE
+todoRoutes.route('/:id').get(function(req, res){
+  let id = req.params.id
+  Todo.findById(id, function(err, todo){
+    res.json(todo)
+  })
+})
+
+//CREATE ROUTE
+todoRoutes.route('/add').post(function(req, res){
+  let todo = new Todo (req.body)
+  todo.save()
+      .then(todo => {
+        res.status(200).json({'todo': 'queaser added succesfully'})
+      })
+      .catch(err => {
+        res.status(400).send('adding new queaser failed')
+      })
+})
+
+//UPDATE ROUTE
+todoRoutes.route('/update/:id').post(function(req, res){
+  Todo.findById(req.params.id, function(err, todo){
+    if(!todo){
+      res.status(404).send('todo not found!')
+    }else {
+      todo.todo_description = req.body.todo_description
+      todo.todo_responsible = req.body.todo_responsible
+      todo.todo_priority = req.body.todo_priority
+      todo.todo_completed = req.body.todo_completed
+
+      todo.save()
+      .then(todo => {
+        res.json('Todo updated')
+      })
+      .catch(err => {
+        res.status(400).send("Oopsi, I'm afraid the update ain't possible")
+      })
+    }
+  })
+})
+
+app.use('/todos', todoRoutes)
 
 app.listen(PORT, function(){
   console.log('server running on port ' + PORT);
